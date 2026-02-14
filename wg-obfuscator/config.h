25d7da1fc13d85351a7bfded7ee1058dfef08042
pegakmop <93389368+pegakmop@users.noreply.github.com>
2026-02-14 17:46:41 +0300

    Delete wg-obfuscator directory

--- wg-obfuscator/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CONFIG_H_
-#define _CONFIG_H_
-
-#include "wg-obfuscator.h"
-
-char *trim(char *s);
-int parse_config(int argc, char **argv, obfuscator_config_t *config);
-
-#endif // _CONFIG_H_
